22dfb51ce973fc0c317874d1c5d09882dd295381
Max <agnusmaximus@berkeley.edu>
2015-05-04 14:15:55 -0700

    make code directory; move everything into it

---
+++ code/matlab/solve.m
@@ -0,0 +1,49 @@
+function [retvalue] = solve(input_directory,output_directory)
+    % Get all files, process in sorted order
+    unsorted_files=dir(fullfile('', strcat(input_directory, '*.in')));
+    files={unsorted_files.name};
+    str=sprintf('%s#',files{:});
+    num=sscanf(str,'%d.in#');
+    [dummy,index]=sort(num);
+    files=files(index);
+    
+    % Process files
+    validationfilepath=strcat(output_directory,'validation_output');
+    totaloutputfilepath=strcat(output_directory,'all_output');
+    totaloutputfile=fopen(totaloutputfilepath,'w');
+    validationfile=fopen(validationfilepath,'w');
+    n_optimal=0;
+    for i=1:length(files)
+        % Debug print
+        filepath=strcat(input_directory,char(files(i)));
+        fprintf('%s\n',strcat('Processing:',filepath));
+        
+        % Individual output file
+        [pathstr,name,ext]=fileparts(char(files(i)));
+        cur_out_file=fopen(strcat(output_directory,name,'.out'),'w');
+        fprintf('%s\n',strcat('Writing To:',strcat(output_directory,name,'.out')));
+
+        
+        % Solve
+        [path,costopt,isopt]=TSP(filepath,0);
+        
+        % Write to outputs
+        g=sprintf('%d ',path);
+        fprintf(totaloutputfile,'%s\n',g);
+        fprintf(cur_out_file,'%s\n',g);
+        fprintf(validationfile,'%g\n',costopt);        
+        
+        % Close file
+        fclose(cur_out_file);
+        
+        % Count optimal answers
+        if isopt==true
+            n_optimal=n_optimal+1;
+        end
+    end
+    fclose(totaloutputfile);
+    fclose(validationfile);
+    
+    % Print statistics
+    fprintf('%d optimal answers out of %d inputs\n',n_optimal,length(files));    
+end
---
+++ code/test_gen/test_gen.py
@@ -0,0 +1,47 @@
+from __future__ import print_function
+import sys
+import random
+
+SMALLEST = 4
+LARGEST = 50
+N = 499
+MAX_VALUE = 100
+MIN_VALUE = 0
+
+output_dir = sys.stdin.readline().strip()
+
+def generate_case(n_cities, index, output_dir_str):
+    print("Case %d" % (index))
+
+    # File to write to
+    if output_dir_str[-1] != "/":
+        output_dir_str += "/"
+    fname = output_dir_str + str(index)
+    f = open(fname+".in", "w")
+
+    # Create adj_mat
+    adj_mat = [[0 for i in range(n_cities)] for j in range(n_cities)]
+    for i in range(n_cities):
+        for j in range(i+1, n_cities):
+            v = random.randint(MIN_VALUE, MAX_VALUE)
+            adj_mat[i][j] = v
+            adj_mat[j][i] = v
+
+    # Create color string
+    colors = ["R"]*(n_cities/2) + ["B"]*(n_cities/2)
+    random.shuffle(colors)
+    colors = "".join(colors)
+
+    # Write adj matrix to file
+    print(n_cities, file=f)
+    for row in adj_mat:
+        for val in row:
+            f.write(str(val) + " ")
+        f.write("\n")
+    print(colors, file=f)
+
+for i in range(N):
+    n_cities = 1
+    while n_cities % 2 == 1:
+        n_cities = random.randint(SMALLEST, LARGEST)
+    generate_case(n_cities, i, output_dir)
